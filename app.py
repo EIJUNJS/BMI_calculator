61fda84462b065bc44bb05dbd6b4f1f2aadd1022
Sherry <shao19980209@gmail.com>
2022-10-20 02:11:15 -0300

    upload the data in database

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ class Data(db.Model):
     email_ = db.Column(db.String(120), unique=True)
     height_ = db.Column(db.Integer)
     weight_ = db.Column(db.Integer)
+    BMI_ = db.Column(db.Float)
 
-    def __init__(self, email_, height_, weight_):
+    def __init__(self, email_, height_, weight_, BMI_):
         self.email_ = email_
         self.height_ = height_
         self.weight_ = weight_
+        self.BMI_ = BMI_
 
 
 with app.app_context():
@@ -36,15 +38,21 @@ def success():
         height = request.form["height_name"]
         weight = request.form["weight_name"]
 
-        BMI_index = float(weight) / pow(float(height) / 100, 2)
+        BMI_index = round(float(weight) / pow(float(height) / 100, 2), 2)
 
-        send_email(email, round(BMI_index, 2))
+        send_email(email, BMI_index)
 
     if db.session.query(Data).filter(Data.email_ == email).count() == 0:
-        data = Data(email, height, weight)
+        data = Data(email, height, weight, BMI_index)
         db.session.add(data)
         db.session.commit()
         return render_template("successful.html")
+    else:
+        data = Data(email, height, weight, BMI_index)
+        db.session.query(Data).filter(Data.email_ == email).update({"weight_": weight})
+        db.session.query(Data).filter(Data.email_ == email).update({"height_": height})
+        db.session.query(Data).filter(Data.email_ == email).update({"BMI_": BMI_index})
+        db.session.commit()
     return render_template("front.html", text="You have uploaded before, we will renew your new data!")
 
 
